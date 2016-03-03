f3b5a99f5081787842f3d873229d26601ae7a679
Christoffer Bro Sørensen <chrisbso@stud.ntnu.no>
2016-03-03 13:27:51 +0100

    DENNEKJØRERJOIKKE

--- hyperbolicPotentialAndField.m
+++ hyperbolicPotentialAndField.m
@@ -2,12 +2,13 @@ function hyperbolicPotentialAndField
 V_DC = 5; %5V
 V_AC = 0; %0V
 r0   = 3; %3mm
-l = 0.01 %steglengde for meshgrid
+l = 0.1; %steglengde for meshgrid
+A = 0.01;
 [X,Y] = meshgrid(-2*r0:l:2*r0,-2*r0:l:2*r0); %%opprett meshgrip for surface plot
 V = V_DC.*(X.^2-Y.^2)./((r0)^2); %% få verdier for V(x,y)
-%figure(1); %% opprett figur
-%surf(X,Y,V); %%plot surface V = V(x,y)
-%colorbar; %%vis colorbar (kanskje unødvendig!)
+figure(1); %% opprett figur
+surf(X,Y,V); %%plot surface V = V(x,y)
+colorbar; %%vis colorbar (kanskje unødvendig!)
 
 [E_x,E_y] = gradient(-V);
 figure(2);
@@ -20,4 +21,15 @@ plot([x_Charge(1),x_Charge(4)],[y_Charge(1),y_Charge(4)],'O','Color','k',...
 plot([x_Charge(2),x_Charge(3)],[y_Charge(2),y_Charge(3)],'O','Color','k',...
     'MarkerSize',20);
 
+V1 = A*log(sqrt((X+r0).^2+(Y).^2));
+V2 = -A*log(sqrt((X).^2+(Y-r0).^2));
+V3 = A*log(sqrt((X-r0).^2+(Y).^2));
+V4 = -A*log(sqrt((X).^2+(Y+r0).^2));
 
+V_tot = V1+V2+V3+V4;
+[E_x_tot,E_y_tot] = gradient(-V_tot);
+figure(3);
+contour(V_tot,30);
+
+% figure(4);
+% streamslice(X,Y,E_x_tot,E_y_tot);
