3fefe5bb3006558b22042bf6ab872e0d53af53aa
Halvorsg <halvor.snersrud@gmail.com>
2016-03-03 19:20:40 +0100

    Har skrevet et lite testscript i main.m
    Det er ikke farlig om dette slettes

--- main.m
+++ main.m
@@ -1,1 +1,7 @@
 %%Mainfila
+[xPos,yPos,xVel,yVel]= forwardEuler(timestep, xPosStart, yPosStart, xVelStart, yVelStart, Vdc, Vac, omega, r0, q, mass, totalTime);
+t = 0:timestep:20*10^-6;
+x = xPosStart*cos(sqrt(Vdc*2*q/(mass*r0^2))*t);
+hold on
+plot(t,x,'r');
+plot(t,xPos(1:length(t)));
