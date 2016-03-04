28d8fb5c1afe4c171d06f6fc2a98887e62d99acc
Halvorsg <halvor.snersrud@gmail.com>
2016-03-04 11:33:27 +0100

    Oppdatering

--- RK4.m
+++ RK4.m
@@ -2,7 +2,7 @@
 function xPos= RK4(timestep,xPosStart,yPosStart,xVelStart, yVelStart,...
     Vdc,Vac,omega,r0,q,mass,totalTime)
 
-totPoints = uint32(totalTime/timestep);
+totPoints = uint32(totalTime/timestep)+1;
 %%u = 1.660539040*10^(-27)
 %%e = 1.60217657*10^(-19) 
 %%Preallokering
@@ -16,13 +16,12 @@ xPos(count) = xPosStart;
 yPos(count) = yPosStart;
 xVel(count) = xVelStart;
 yVel(count) = yVelStart;
-hold on
+
 for time=0:timestep:totalTime
     count = count+1;
-    xPos(count) = RK4Step...
-        (timestep,xPos(count-1),yPos(count-1),xVel(count-1),yVel(count-1),...
+    xPos(count) = RK4Step(timestep,xPos(count-1),yPos(count-1),xVel(count-1),yVel(count-1),...
         Vdc,Vac,omega,time,r0,q,mass);
-    plot(count*timestep,xPos)
+
 end
 %%plot(xPos*10^3,yPos*10^3,'r');
 
--- RK4Step.m
+++ RK4Step.m
@@ -2,11 +2,11 @@
 function xPos = RK4Step(timestep,xPosStart,yPosStart,...
     xVelStart, yVelStart,Vdc,Vac,omega,t,r0,q,mass)
 h = timestep;
-xVel = xVelStart + timestep*electricForceX(Vdc,Vac,omega,t,xPosStart,r0,q)/mass;
+xVel = xVelStart + h*electricForceX(Vdc,Vac,omega,t,xPosStart,r0,q)/mass;
 s1 = xVel + t*electricForceX(Vdc,Vac,omega,t,xPosStart,r0,q)/mass;
 s2 = xVel + (t+h/2)*electricForceX(Vdc,Vac,omega,t+h/2,xPosStart+h/2*s1,r0,q)/mass;
 s3 = xVel + (t+h/2)*electricForceX(Vdc,Vac,omega,t+h/2,xPosStart+h/2*s2,r0,q)/mass;
 s4 = xVel + (t+h)*electricForceX(Vdc,Vac,omega,t+h,xPosStart+h*s3,r0,q)/mass;
-xPos = xPosStart + h/6*(s1+h/2*s2 + h/2*s3 +s4);
+xPos = xPosStart + h/6*(s1+2*s2 + 2*s3 +s4);
 
 end    
