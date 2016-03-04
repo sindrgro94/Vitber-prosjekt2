8bee3c94ed4429f53a2012d7517103bb19690255
Christoffer Bro Sørensen <chrisbso@stud.ntnu.no>
2016-03-04 12:40:32 +0100

    xVel regnet ut for RK4

--- RK4Step.m
+++ RK4Step.m
@@ -7,5 +7,11 @@ s2 = xVelStart + (h)*electricForceX(Vdc,Vac,omega,t+h/2,xPosStart+h/2*s1,r0,q)/m
 s3 = xVelStart + (h)*electricForceX(Vdc,Vac,omega,t+h/2,xPosStart+h/2*s2,r0,q)/mass;
 s4 = xVelStart + (h)*electricForceX(Vdc,Vac,omega,t+h,xPosStart+h*s3,r0,q)/mass;
 xPos = xPosStart + h/6*(s1+2*s2 + 2*s3 +s4);
-xVel = xVelStart + h*electricForceX(Vdc,Vac,omega,t,xPosStart,r0,q)/mass;
+
+
+xVel1 = electricForceX(Vdc,Vac,omega,t,xPosStart,r0,q)/mass;
+xVel2 = electricForceX(Vdc,Vac,omega,t+h/2,xPosStart+h/2*s1,r0,q)/mass;
+xVel3 = electricForceX(Vdc,Vac,omega,t+h/2,xPosStart+h/2*s2,r0,q)/mass;
+xVel4 = electricForceX(Vdc,Vac,omega,t+h,xPosStart+h*s3,r0,q)/mass;
+xVel = xVelStart + h/6*(xVel1 + 2*xVel2 + 2*xVel3 + xVel4);
 end    
