570e25fa943ab844435046864429c8fe0ab67b49
sindrgro94 <sindre.grostad@icloud.com>
2016-03-04 10:40:39 +0100

    main

--- main.m
+++ main.m
@@ -1,19 +1,6 @@
 %%Mainfila
-timestep = 0.003 * 10^(-7);
-xPosStart = 1*10^(-3);
-yPosStart = 0;
-xVelStart = 0;
-yVelStart = 0;
-Vdc = 5;
-Vac = 0;
-omega = 0;
-r0 = 0.0030;
-q = 1.60217657*10^(-19);
-mass = 28*1.660539040*10^(-27);
-totalTime = 20*10^(-6);
-[xPos,yPos,xVel,yVel]= forwardEuler(timestep, xPosStart, yPosStart, xVelStart, yVelStart, Vdc, Vac, omega, r0, q, mass, totalTime);
-t = 0:timestep:20*10^-6;
-x = xPosStart*cos(sqrt(Vdc*2*q/(mass*r0^2))*t);
-hold on
-plot(t,x,'r');
-plot(t,xPos(1:length(t)));
+alternative = menu('Hva vil du gj?re?','Teste Euler mot Analytisk l?sning');
+switch alternative
+    case 1
+    testEulerVSAnalytic;
+end
