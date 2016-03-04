751e082410d30e4d9aa10e7711e310f7a3c7eccf
sindrgro94 <sindre.grostad@icloud.com>
2016-03-04 13:32:16 +0100

    voltage test

--- RK4success.m
+++ RK4success.m
@@ -1,7 +1,14 @@
 %% Returnerer 1 om den kom gjennom og 0 ellers
 function success = RK4success(timestep,xPosStart,yPosStart,...
     xVelStart, yVelStart,zVelStart,Vdc,Vac,omega,r0,q,mass)
-    
+    [xPos,yPos,zPos] = RK4(timestep,xPosStart,yPosStart,xVelStart, yVelStart,...
+    zVelStart,Vdc,Vac,omega,r0,q,mass,totalTime);
+    pos = sqrt(xPos^2+yPos^2);
+    if pos<r0/2
+        success = 1;
+    else
+        success = 0;
+    end
     
 
 end
