19750bc651f4adb7241ea0a6e60543f73c0f0c2e
sindrgro94 <sindre.grostad@icloud.com>
2016-03-07 15:22:38 +0100

    optimalisering RK4Voltage og grafer

--- TestRK4Voltage.m
+++ TestRK4Voltage.m
@@ -3,19 +3,28 @@ function TestRK4Voltage()
 [timestep,xPosStart,yPosStart,xVelStart, yVelStart,...
     zVelStart,Vdc,Vac,omega,r0,q,mass,totalTime] = getVariables();
 voltageStep = 0.5;
+mass = mass/2;
+VacStart = 5;
 counter = 1;
 for Vdc = 1:voltageStep:10
-    for Vac = 5:1:60
+    first = 1; %%optimalisering for ? slippe ? kj?re RK4 s? mange ganger
+    for Vac = VacStart:1:60
         success = RK4success(timestep,xPosStart,yPosStart,...
         xVelStart, yVelStart,zVelStart,Vdc,Vac,omega,r0,q,mass,totalTime);
         if success == 1
             yAxes(counter) = Vdc;
             xAxes(counter) = Vac;
             counter = counter + 1;
+            if first == 1
+                VacStart = Vac;
+                first = 0;
+            end
         end
     end
 end
 plot(xAxes,yAxes,'x');
 ylabel('Vdc');
 xlabel('Vac');
+xlim([5 60]);
+ylim([1 10]);
 end
--- plotStableAndUnstable.m
+++ plotStableAndUnstable.m
@@ -2,7 +2,7 @@
 [timestep,xPosStart,yPosStart,xVelStart, yVelStart,...
 zVelStart,Vdc,Vac,omega,r0,q,mass,totalTime] = getVariables();
 %%stabil bane:
-Vdc = 5;
+Vdc = 1;
 Vac = 45;
 [xPos,yPos,zPos] = RK4(timestep,xPosStart,yPosStart,xVelStart, yVelStart,...
 zVelStart,Vdc,Vac,omega,r0,q,mass,totalTime);
@@ -14,3 +14,17 @@ hold on
 plot(zPos,xPos,'b');
 plot(zPos,yPos,'r');
 legend('x-posisjon','y-posisjon');
+%% Ustabil bane:
+Vdc = 8;
+Vac = 40;
+[xPos,yPos,zPos] = RK4(timestep,xPosStart,yPosStart,xVelStart, yVelStart,...
+zVelStart,Vdc,Vac,omega,r0,q,mass,totalTime);
+figure(2);
+title('Ustabil bane');
+xlabel('Distanse z-aksen [m]');
+ylabel('Distanse x/y-aksen [m]');
+hold on
+plot(zPos,xPos,'b');
+plot(zPos,yPos,'r');
+legend('x-posisjon','y-posisjon');
+ylim([-0.004 0.004]);
