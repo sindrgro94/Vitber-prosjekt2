ab745b35dce3a87adba679985271bd906e722a67
sindrgro94 <sindre.grostad@icloud.com>
2016-03-07 15:41:58 +0100

    rydding og banegrafer

--- plotStableAndUnstable.m
+++ plotStableAndUnstable.m
@@ -1,12 +1,13 @@
 %%plotStableAndUnstable
+close all
 [timestep,xPosStart,yPosStart,xVelStart, yVelStart,...
 zVelStart,Vdc,Vac,omega,r0,q,mass,totalTime] = getVariables();
 %%stabil bane:
-Vdc = 1;
-Vac = 45;
+Vdc = 3;
+Vac = 40;
 [xPos,yPos,zPos] = RK4(timestep,xPosStart,yPosStart,xVelStart, yVelStart,...
 zVelStart,Vdc,Vac,omega,r0,q,mass,totalTime);
-figure(1);
+f1 = figure(1);
 title('Stabil bane');
 xlabel('Distanse z-aksen [m]');
 ylabel('Distanse x/y-aksen [m]');
@@ -14,12 +15,13 @@ hold on
 plot(zPos,xPos,'b');
 plot(zPos,yPos,'r');
 legend('x-posisjon','y-posisjon');
+ylim([-0.003 0.003]);
 %% Ustabil bane:
-Vdc = 8;
-Vac = 40;
+Vdc = 5;
+Vac = 30;
 [xPos,yPos,zPos] = RK4(timestep,xPosStart,yPosStart,xVelStart, yVelStart,...
 zVelStart,Vdc,Vac,omega,r0,q,mass,totalTime);
-figure(2);
+f2 = figure(2);
 title('Ustabil bane');
 xlabel('Distanse z-aksen [m]');
 ylabel('Distanse x/y-aksen [m]');
@@ -27,4 +29,8 @@ hold on
 plot(zPos,xPos,'b');
 plot(zPos,yPos,'r');
 legend('x-posisjon','y-posisjon');
-ylim([-0.004 0.004]);
+ylim([-0.003 0.003]);
+
+%% kun ved lagring av filer:
+saveTightFigure(f1,'Stabil bane')
+saveTightFigure(f2,'Ustabil bane')
