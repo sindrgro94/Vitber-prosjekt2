b1db6f44b1659b33844b922df9b3114a6466beb0
Christoffer Bro Sørensen <chrisbso@stud.ntnu.no>
2016-03-08 13:51:25 +0100

    oppgave2ferdig

--- plotPotentialAndField.m
+++ plotPotentialAndField.m
@@ -11,42 +11,51 @@ y_Charge = [0,r0,-r0,0]; %%....
 
 %--------------------------------------------------------------------%
 
-figure(1); %% konturplot for hyperbolsk potensial
+f1 = figure(1); %% konturplot for hyperbolsk potensial
 V = V_DC.*(X.^2-Y.^2)./((r0)^2); %% f? verdier for V(x,y) hyperbolsk
-contour(V,50); %%plot contour of V = V(x,y)
+contour(V,30); %%plot contour of V = V(x,y)
 colorbar; %%vis colorbar (kanskje un?dvendig!)
-
+box off;
+set(gca,'visible','off');
+saveTightFigure(f1,'konturHyperbolsk.pdf');
 %--------------------------------------------------------------------%
 
-figure(2); %%feltlinjer for hyperbolsk elektrisk felt
+f2 = figure(2); %%feltlinjer for hyperbolsk elektrisk felt
 hold on;
 [E_x,E_y] = gradient(-V);
-streamslice(X,Y,E_x,E_y,10);
+streamslice(X,Y,E_x,E_y,5);
 plot([x_Charge(1),x_Charge(4)],[y_Charge(1),y_Charge(4)],'O','Color','k',...
     'MarkerSize',20); %% plotter ladningene langs x-aksen (som 'O')
 plot([x_Charge(2),x_Charge(3)],[y_Charge(2),y_Charge(3)],'O','Color','k',...
     'MarkerSize',20); %% plotter ladningene langs y-aksen (som 'O')
+set(gca,'visible','off');
+saveTightFigure(f2,'feltlinjerHyperbolsk.pdf')
 
 %--------------------------------------------------------------------%
 
-figure(3); %%konturplot for kvadrupolens potensial (V ~ln(r))
+f3 = figure(3); %%konturplot for kvadrupolens potensial (V ~ln(r))
 V1 = A*log(sqrt((X+r0).^2+(Y).^2));
 V2 = -A*log(sqrt((X).^2+(Y-r0).^2));
 V3 = A*log(sqrt((X-r0).^2+(Y).^2));
 V4 = -A*log(sqrt((X).^2+(Y+r0).^2));
 V_tot = V1+V2+V3+V4; %%bruker superposisjon av potensialet
 [E_x_tot,E_y_tot] = gradient(-V_tot);
-contour(V_tot,50);
+contour(V_tot,30);
+box off;
+set(gca,'visible','off');
+saveTightFigure(f3,'konturKvadrupol.pdf')
 
 %--------------------------------------------------------------------%
 
-figure(4); %%feltlinjer for kvadrupolens elektriske felt (E ~1/r)
- streamslice(x,y,E_x_tot,E_y_tot,10);
+f4 = figure(4); %%feltlinjer for kvadrupolens elektriske felt (E ~1/r)
+ streamslice(x,y,E_x_tot,E_y_tot,5);
  hold on;
 plot([x_Charge(1),x_Charge(4)],[y_Charge(1),y_Charge(4)],'O','Color','k',...
     'MarkerSize',20); %%plotter ladningene langs x-aksen
 plot([x_Charge(2),x_Charge(3)],[y_Charge(2),y_Charge(3)],'O','Color','k',...
     'MarkerSize',20); %%plotter ladningene langs y-asken
+set(gca,'visible','off');
+saveTightFigure(f4,'feltlinjerKvadrupol.pdf')
 
 %--------------------------------------------------------------------%
 end
