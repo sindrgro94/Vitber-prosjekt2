e0c05f3279b6ac23109c7e056a9c9e9f869a0e7c
Halvorsg <halvor.snersrud@gmail.com>
2016-03-11 14:07:57 +0100

    hahaha

--- plotAntallGjennom.m
+++ plotAntallGjennom.m
@@ -1,13 +1,15 @@
 %Plotte alle partiklene som kommer gjennom som funksjon av forskjellige
 %masser
 function plotAntallGjennom()
-prosentGjennom=zeros(1,3);
+%Kjør fra 27.6 og til 28.6 med mange verdier. Anbefaler å kjøre mellom 50
+%og 100 verdier. Bruk et massesteg på 0.01 eller 0.05
+%prosentGjennom=zeros(1,20);
 j = 1;
-for i = 27:0.1:29
-    prosentGjennom(j) = randomSuccess(i,5,7.65,46);
+for i = 27.6:0.1:28.6
+    prosentGjennom(j) = randomSuccess(i,20,7.65,46);
     j = j+1;
 end
 
-t = 27:0.1:29;
+t = 27.6:0.1:28.6;
 plot(t,prosentGjennom) %Plotter prosentandelen som kom frem til sensoren
 ylim([0,1])
